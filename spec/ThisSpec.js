af4590f892a318a32a3ddcaffe9ed6eeda6d1730
Piotr Repetowski <piotr.repetowski@gmail.com>
2013-09-08 01:28:49 +0200

    More investigation on this keyword

--- spec/ThisSpec.js
+++ spec/ThisSpec.js
@@ -1,4 +1,4 @@
-describe('this keyword', function() {
+describe('this', function() {
     beforeEach(function() {
         attribute = undefined;    
     });
@@ -32,5 +32,61 @@ describe('this keyword', function() {
         var newObject = new object.setAttribute(value);
         expect(newObject.attribute).toBe(value);
         expect(object.attribute).toBeUndefined();
+    });
+
+    it('is set to specified object when function is passed as apply function parameter: fooFunction.apply(object);', function() {
+        var objectA = new AnObject();
+        var objectB = new AnObject();
+        var value = 19;
+        objectA.setAttribute.apply(objectB, [value]);
+        expect(objectA.attribute).toBeUndefined();
+        expect(objectB.attribute).toBe(value);
+    });
+
+    it('is set to specified object when function is passed as call function parameter: fooFunction.call(object);', function() {
+        var objectA = new AnObject();
+        var objectB = new AnObject();
+        var value = 19;
+        objectA.setAttribute.call(objectB, value);
+        expect(objectA.attribute).toBeUndefined();
+        expect(objectB.attribute).toBe(value);
+    });
+
+    it('is window when set to null', function() {
+        var object = new AnObject();
+        expect(object.getThis.call(null)).toBe(window);
+        expect(object.getThis.apply(null)).toBe(window);
+    });
+
+    it('is window when set to undefined', function() {
+        var object = new AnObject();
+        expect(object.getThis.call(undefined)).toBe(window);
+        expect(object.getThis.apply(undefined)).toBe(window);
+    });
+
+    it('is object even for primitive types', function() {
+        var object = new AnObject();
+        expect(typeof object.getThis.call(1)).toEqual('object');
+        expect(typeof object.getThis.call(true)).toEqual('object');
+        expect(typeof object.getThis.call("foo")).toEqual('object');
+    });
+
+    it('is window in indirect eval call', function() {
+        expect((1, eval)('this')).toBe(window);
+    });
+
+    it('is current this in direct eval call', function() {
+        expect(eval('this')).toBe(this);
+    });
+
+    it('can be binded using bind function', function() {
+        var objectA = new AnObject();
+        var objectB = new AnObject();
+        var bindedGetThis = objectA.getThis.bind(objectB);
+        expect(bindedGetThis()).toBe(objectB);
+        expect(bindedGetThis.call(objectA)).toBe(objectB);
+        expect(bindedGetThis.apply(objectA)).toBe(objectB);
+        objectA.bindedGetThis = bindedGetThis;
+        expect(objectA.bindedGetThis()).toBe(objectB);
     });
 });
