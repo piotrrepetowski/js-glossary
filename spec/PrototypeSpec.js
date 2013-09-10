3c38861f596b9b91e756c699d0892c8d0276c2ce
Piotr Repetowski <piotr.repetowski@gmail.com>
2013-09-11 00:32:07 +0200

    Prototype specification

--- spec/PrototypeSpec.js
+++ spec/PrototypeSpec.js
@@ -1,73 +1,109 @@
 describe('prototype', function() {
-    it('is property of function but this is not internal [[Prototype]]', function() {
-        var foo = function() {};
-        expect(foo.prototype).not.toEqual(foo.__proto__);
-    });
+    describe('Object internal [[Prototype]] attribute', function() {
+        it('is accessible through Object.getPrototypeOf()', function() {
+            expect(Object.getPrototypeOf({})).toEqual({});
+        });
 
-    it('in object is set from construct function.prototype attribute value', function() {
-        var Foo = function() {};
-        var object = new Foo();
-        expect(object.__proto__).toBe(Foo.prototype);
-    });
+        it('is set to value of Object.prototype for new objects', function() {
+            expect(Object.getPrototypeOf({})).toBe(Object.prototype);
+        });
 
-    it('points the object parent', function() {
-        var literalObject = {};
-        expect(literalObject.__proto__).toEqual(Object.prototype);
-    });
-
-    it('allows to add new behavior to existing objects', function() {
-        var Foo = function() {};
-        var object = new Foo();
-        var value = 'Bar';
-        Foo.prototype.getBar = function() {
-            return value;
-        };
-        expect(object.getBar()).toEqual(value);
-    });
-
-    it('canont be replaced globally', function() {
-        var Foo = function() {};
-        var objectA = new Foo();
-        expect(objectA.x).toBeUndefined();
-        var Bar = function() {
-            this.x = 24;
-        };
-        Foo.prototype = new Bar();
-        var objectB = new Foo();
-        expect(objectA.x).toBeUndefined();
-        expect(objectB.x).toBeDefined();
-    });
-
-    it('in function has property called constructor pointing to function itself', function() {
-        var Foo = function() {};
-        expect(Foo.prototype.constructor).toBe(Foo);
-    });
-
-    it('in objects is not accessible through prototype property', function() {
-        expect({}.prototype).toBeUndefined();
-    });
-
-    it('in objects is accessible through non standard __proto__ property', function() {
-        expect({}.__proto__).toBeDefined();
-    });
-
-    it('in objects is accessible through Object.getPrototypeOf()', function() {
-        expect(Object.getPrototypeOf({})).toBeDefined();
-    });
-
-    xit('can be used to share common behavior', function() {
+        it('is set to null for Object.prototype', function() {
+            expect(Object.getPrototypeOf(Object.prototype)).toBe(null);
+        });
     
+        it('is set to Function.prototype for new functions', function() {
+            var foo = function() {};
+            expect(Object.getPrototypeOf(foo)).toEqual(Function.prototype);
+        });
+
+        it('is set to Object for Function.prototype', function() {
+            expect(Object.getPrototypeOf(Function.prototype)).toEqual(jasmine.any(Object));
+        });
+
+        it('is set to Bar if Foo inherits from Bar', function() {
+            var person = {};
+
+            var fooValue = 'bar';
+
+            var alien = {
+                foo: fooValue
+            };
+
+            // __proto__ is non standard attribute
+            person.__proto__ = alien;
+
+            expect(Object.getPrototypeOf(person)).toEqual(alien);
+        });
+
+        it('can be verified through isPrototypeOf', function() {
+            var person = {};
+
+            var fooValue = 'bar';
+
+            var alien = {
+                foo: fooValue
+            };
+
+            // __proto__ is non standard attribute
+            person.__proto__ = alien;
+
+            expect(alien.isPrototypeOf(person)).toBe(true);
+        });
+
+        it('is used to find properties in objects parents', function() {
+            var familyValue = 'foo';
+
+            var grandfather = { family: familyValue };
+
+            var father = {};
+            // __proto__ is non standard attribute
+            father.__proto__ = grandfather;
+
+            var son = {};
+            // __proto__ is non standard attribute
+            son.__proto__ = father;
+
+            expect(son.family).toBe(familyValue);
+        });
+
+        it('can be replaced dynamically', function() {
+            var familyValue = 'foo';
+
+            var grandfather = {};
+
+            var father = {};
+            // __proto__ is non standard attribute
+            father.__proto__ = grandfather;
+
+            var son = {};
+            // __proto__ is non standard attribute
+            son.__proto__ = father;
+
+            expect(son.family).toBeUndefined();
+
+            grandfather.family = familyValue;
+
+            expect(son.family).toBe(familyValue);
+        });
     });
 
-    xit('is a property of all objects', function() {
-    
-    });
+    describe('Function prototype attribute', function() {
+        it('is an attribute of each funciton', function() {
+            var foo = function() {};
+            expect(foo.prototype).toBeDefined();
+        });
 
-    xit('is chained', function() {
-    
-    });
+        it('is not a function internal [[Prototype]]', function() {
+            var foo = function() {};
+            expect(Object.getPrototypeOf(foo)).not.toEqual(foo.prototype);
+        });
 
-    xit('allows to call parent function from child', function() {
-    
+        it('is used to set internal [[Prototype]] for objects created by function', function() {
+            var Foo = function() {};
+            var fooObject = new Foo();
+
+            expect(Object.getPrototypeOf(fooObject)).toBe(Foo.prototype);
+        });
     });
 });
