3000eba3007c4f5eb907c56ca06d1a6e3640b5d6
mirfilip <mirfilip@gmail.com>
2013-09-12 15:55:09 +0200

    TypeOf operator with primitives (3 ways)

--- spec/TypeOfSpec.js
+++ spec/TypeOfSpec.js
@@ -10,6 +10,28 @@ describe('TypeOf should be used for primitive types ONLY ! (except null :D)', fu
 		expect(typeof function(){}).toEqual('function');
 	});
 
+	it('when used with new operator it wraps primitive with object !', function(){
+		// using primitives
+		expect(typeof true).toEqual('boolean');
+		expect(typeof "abc").toEqual('string');
+		expect(typeof 123.toEqual('number');
+			
+		// using constructor function
+		expect(typeof Boolean(true)).toEqual('boolean');
+		expect(typeof String("abc")).toEqual('string');
+		expect(typeof Number(123).toEqual('number');
+
+		// new operator causes JS to create empty object and map constructor function return value to empty object property
+		expect(typeof new Boolean(true)).not.toEqual('boolean');
+		expect(typeof new Boolean(true)).toEqual('object');
+
+		expect(typeof new String("abc")).not.toEqual('string');
+		expect(typeof new String("abc")).toEqual('object');
+
+		expect(typeof new Number(123)).not.toEqual('number');
+		expect(typeof new Number(123)).toEqual('object');
+	});
+
 	it('has some strange behaviour too', function(){
 		expect(typeof null).not.toEqual('null');
 		expect(typeof null).not.toEqual('undefined');
