d0f8b88461fee7cdda4848cbafc3fd11945e401d
Piotr Repetowski <piotr.repetowski@gmail.com>
2013-09-12 21:44:01 +0200

    Travis - testing

--- CI/karma.config.js
+++ CI/karma.config.js
@@ -1,25 +1,16 @@
-  
-  module.exports = function(config) {
-    config.set({
-      basePath: '.',
-      frameworks: ['jasmine'],
-      files: [
-      'spec/ThisSpec.js',
-      'spec/ObjectsSpec.js',
-      'spec/PrototypeSpec.js',
-      'spec/TypeOfSpec.js',
-      'spec/InstanceOfSpec.js',
-      'spec/ConstructorSpec.js',
-      'spec/ConstructorValidationSpec.js',
-      'spec/CustomMatchersSpec.js'
-      ],
-      reporters: ['dots'],
-      port: 9876,
-      colors: false,
-      logLevel: config.LOG_INFO,
-      autoWatch: false,
-      // browsers: ['PhantomJS'],
-      captureTimeout: 60000
-      // singleRun: true
-    });
-  };
+module.exports = function(config) {
+  config.set({
+    basePath: '.',
+    frameworks: ['jasmine'],
+    files: [
+      'src/**/*.js',
+      'spec/**/*Spec.js'
+    ],
+    reporters: ['dots'],
+    port: 9876,
+    colors: false,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    captureTimeout: 60000
+  });
+};
