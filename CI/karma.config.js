c4cd85e2d945c8fa8b3690dc12bec3b93aa5bca7
mirfilip <mirfilip@gmail.com>
2013-09-16 12:04:22 +0200

    Karma config fix for jasmine-addons + new karma config spec

--- CI/karma.config.js
+++ CI/karma.config.js
@@ -1,26 +1,20 @@
-basePath = '..';
-
-files = [
-    JASMINE,
-    JASMINE_ADAPTER,
-    'src/**/*.js',
-    'spec/**/*Spec.js'
-];
-
-reporters = ['dots'];
-
-port = 9876;
-
-runnerPort = 9100;
-
-colors = true;
-
-logLevel = LOG_INFO;
-
-autoWatch = true;
-
-browsers = ['Firefox'];
-
-captureTimeout = 60000;
-
-singleRun = false;
+module.exports = function(config) {
+    config.set({
+	basePath: '..',
+	files: [
+		'lib/jasmine-1.3.1/jasmine-addons.js',
+	    'src/**/*.js',
+	    'spec/**/*Spec.js'
+	],
+	frameworks: ['jasmine'],
+	reporters: ['progress'],
+	port: 9876,
+	runnerPort: 9100,
+	colors: true,
+	logLevel: config.LOG_INFO,
+	autoWatch: true,
+	browsers: ['Firefox'],
+	captureTimeout: 60000,
+	singleRun: false
+    });
+  };
